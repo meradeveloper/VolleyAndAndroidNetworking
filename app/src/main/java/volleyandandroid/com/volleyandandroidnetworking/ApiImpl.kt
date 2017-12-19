16df716ade193072a82481f76537e97211e37d08
meradeveloper <rawatsumit115@gmail.com>
2017-12-19 16:19:37 +0530

    first

---
+++ app/src/main/java/volleyandandroid/com/volleyandandroidnetworking/ApiImpl.kt
@@ -0,0 +1,36 @@
+package learnkotlin.com.learnkotlin.Common.WebApiModule
+
+import android.content.Context
+import org.json.JSONObject
+
+
+/**
+ * Created by Localadmin on 11/15/2017.
+ */
+  open class ApiImpl (context:Context) {
+
+
+    init {
+        //VolleyApi.apiPrsntr=apiPrsntr
+        VolleyApi.context=context
+    }
+
+    companion object {
+        var URL:String=""
+        var JSONOBJECTBODY:JSONObject?=null
+
+    }
+
+    fun addCallback(impl: ApiPrsntr):CallingMethods
+    {
+        VolleyApi.apiPrsntr=impl
+        return CallingMethods()
+    }
+
+    open class CallingMethods
+    { fun callWithAndroidNetworking()= AndroidNetworkingApi.AURL()
+      fun callWithVolley() = VolleyApi.VURL()
+    }
+
+
+}
