0d6ee4eb2e7521a619d483850dfccfdc83077b75
meradeveloper <rawatsumit115@gmail.com>
2017-12-20 12:48:16 +0530

    second

--- volleyandandroidinone/src/main/java/volleyandandroid/com/volleyandandroidinone/ApiPrsntr.kt
+++
@@ -1,9 +0,0 @@
-package learnkotlin.com.learnkotlin.Common.WebApiModule
-
-/**
- * Created by Localadmin on 11/15/2017.
- */
-interface ApiPrsntr {
-    fun onSuccesResponse(ResponseInString:String,url:String)
-    fun onFailedResponse(error: String)
-}
---
+++ volleyandandroidinone/src/main/java/volleyandandroid/com/volleyandandroidinone/Name.java
@@ -0,0 +1,13 @@
+package volleyandandroid.com.volleyandandroidinone;
+
+/**
+ * Created by Localadmin on 12/20/2017.
+ */
+
+public class Name {
+
+    public static String getName(String Name)
+    {
+        return " Hello "+Name;
+    }
+}
